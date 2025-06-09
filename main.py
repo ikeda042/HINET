c76e268b644b9c2d5230d01f2b1a82deac46428f
ikeda042 <ikeda042main@gmail.com>
2025-06-09 17:16:14 +0900

    int

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ import asyncio
 import aiohttp
 import os
 from slack import send_slack, SlackMessage
-
+import socket
 
 load_dotenv()
 MAC_ADDRESSES = os.getenv("MAC_ADDRESSES", "").split(",")
 MY_MAC_ADDRESS = os.getenv("MY_MAC_ADDRESS", "")
-async def check_internet_connection() -> bool | aiohttp.ClientResponse:
+
+def check_internet_connection(timeout: float = 3.0) -> bool:
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get("https://www.msftconnecttest.com/connecttest.txt", timeout=5) as response:
-                return response.status == 200
-    except:
+        # create_connection は成功するとソケットを返す
+        socket.create_connection(("1.1.1.1", 443), timeout=timeout).close()
+        return True
+    except OSError:
         return False
     
 async def hinet():
@@ -27,15 +28,14 @@ async def hinet():
 
 async def main():
     while True:
-        if await check_internet_connection():
+        if check_internet_connection():
             print("Internet connection is available.")
             await send_slack(
                 webhook_url=os.getenv("WEBHOOK_URL"),
                 payload=SlackMessage(text="Internet connection is available. Starting HINET login process.")),
-        
         else:
             print("No internet connection. Retrying in 10 seconds...")
-            await hinet()
+            # await hinet()
         await asyncio.sleep(120)
 
 if __name__ == "__main__":
