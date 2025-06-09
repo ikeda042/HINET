f15f61dda49124da25fbf18e2bd3067cad9f885f
ikeda042 <ikeda042main@gmail.com>
2025-06-09 16:03:41 +0900

    instance化

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ async def main():
     mac_addresses = []
     for mac_address in mac_addresses:
         try:
-            hinet_login = HINETLogin(mac_address=mac_address)
-            await hinet_login.main()
+            await HINETLogin(mac_address=mac_address).main()
         except Exception as e:
             print(e)
             pass
