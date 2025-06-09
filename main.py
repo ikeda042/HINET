01388888f33f8885384cc705cc94751e8e0d9890
ikeda042 <ikeda042main@gmail.com>
2025-06-09 16:19:19 +0900

    print

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ async def check_internet_connection() -> bool | aiohttp.ClientResponse:
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get("http://www.msftconnecttest.com/connecttest.txt", timeout=5) as response:
-                print(f"Response status: {response.status}")
                 return response.status == 200
     except:
         return False
@@ -21,12 +20,10 @@ async def hinet():
         try:
             await HINETLogin(mac_address=mac_address).main()
         except Exception as e:
-            print(e)
             pass
 
 async def main():
     while True:
-        print("Checking internet connection...")
         if await check_internet_connection():
             print("Internet connection is available.")
         else:
